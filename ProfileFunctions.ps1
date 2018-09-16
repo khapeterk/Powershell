4ae732e648e0b95a988755b65fdfe550d2b4a7b6
Peter Kha <khapeterk@gmail.com>
2018-09-16 13:39:37 -0400

    function financial projection angular

--- ProfileFunctions.ps1
+++ ProfileFunctions.ps1
@@ -2,6 +2,10 @@ function SpiritIslandAngular {
   cd c:\LocalRepo\SpiritIsland\angular
 }
 
+function FinancialProjectionAngular {
+  cd C:\LocalRepo\FinancialProjection\FinancialProjectionAngular
+}
+
 function testonce {
   ng test --watch=false
 }
